fec5d228b1cf04f3e624d9d4d283d84ff5bdc36c
Rnmoon <aryansunilmoon@gmail.com>
2025-04-10 22:30:30 +0530

    Adding Authentication

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+        http2: false,
+        os: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
